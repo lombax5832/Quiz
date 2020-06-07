9f0b6f7705b8c861329265eed1fed41b0cf6e8e0
Alexei Snytkine <ipodalexei@gmail.com>
2020-06-06 21:44:45 -0400

    display quizzes grouped under their categories

--- src/components/editquestion/questionmeta.tsx
+++ src/components/editquestion/questionmeta.tsx
@@ -1,40 +1,81 @@
-import { Card } from '@material-ui/core';
+import { Card, TextField } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import CardActions from '@material-ui/core/CardActions';
 import Grid from '@material-ui/core/Grid';
 import React from 'react';
+import { ICategoryWithQuizzes } from '../../interfaces/ICategory';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import IQuiz from '../../interfaces/IQuiz';
+import categoryform from '../editcategory/categoryform';
 
 
-const QuestionMeta = (props: {reset: Function}) => {
+const QuestionMeta = (props: { reset: Function, quizzes: Array<ICategoryWithQuizzes> }) => {
+
+  interface IQuizWithCategory extends IQuiz {
+    categoryName: string
+  }
+
+  const [value, setValue] = React.useState<IQuizWithCategory | null>(null);
+
+  const categories = props.quizzes.map((category) => {
+    const quizzes = category.quizzes.map((quiz) => {
+      return {
+        ...quiz,
+        categoryName: category.title
+      }
+    })
+    return quizzes;
+
+  }).flat();
+
+  console.log("Categories: ", categories)
 
   return (
-      <Card>
-        <CardActions>
-          <Grid
-              container
-              spacing={2}
-              direction="row"
-              justify="flex-end"
-              alignItems="stretch"
-          >
-
+    <Card>
+      <CardActions>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justify="flex-end"
+          alignItems="stretch"
+        >
+          {
             <Grid item>
-              <Button
-                  variant="outlined"
-                  color="default"
-                  onClick={() => props.reset()}>
-                Reset Values
-              </Button>
+              <Autocomplete
+                id="grouped-demo"
+                options={categories}
+                groupBy={(option) => option.categoryName}
+                getOptionLabel={(option) => option.title}
+                style={{ width: 300 }}
+                disableClearable
+                renderInput={(params) => <TextField {...params} label="Quiz" variant="outlined" />}
+                getOptionSelected={(option, value) => option._id === value._id}
+                value={value}
+                onChange={(event: any, newValue: IQuizWithCategory | null) => {
+                  setValue(newValue)
+                  console.log("Autocomplete", value)
+                }}
+              />
             </Grid>
-            <Grid item>
-              <Button variant="outlined" type="submit" color="primary">
-                Submit
+          }
+          <Grid item>
+            <Button
+              variant="outlined"
+              color="default"
+              onClick={() => props.reset()}>
+              Reset Values
               </Button>
-            </Grid>
           </Grid>
-        </CardActions>
+          <Grid item>
+            <Button variant="outlined" type="submit" color="primary">
+              Submit
+              </Button>
+          </Grid>
+        </Grid>
+      </CardActions>
 
-      </Card>
+    </Card>
   );
 };
 
--- src/components/views/editquiz/editquizlist.tsx
+++ src/components/views/editquiz/editquizlist.tsx
@@ -1,41 +1,113 @@
-import { Button, Card, CardActions, CardContent, makeStyles, Typography } from '@material-ui/core';
-import React from 'react';
+import { Button, ButtonGroup, Card, CardActions, CardContent, ExpansionPanelDetails, ExpansionPanelSummary, List, ListItem, ListItemSecondaryAction, ListItemText, makeStyles, Typography, withStyles } from '@material-ui/core';
+import MuiExpansionPanel from '@material-ui/core/ExpansionPanel';
+import { Delete, Edit, ExpandMore } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
+import HttpClient from '../../../httpclient/client';
+import LinearLoader from '../../loadingbar';
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
+  },
+  list: {
+    width: "100%",
   },
   bullet: {
     display: 'inline-block',
     margin: '0 2px',
     transform: 'scale(0.8)',
   },
-  title: {
-    fontSize: 14,
+  quizName: {
+    fontSize: "100%",
   },
   pos: {
     marginBottom: 12,
   },
+  divider: {
+    marginTop: 10,
+  },
 });
+
+const ExpansionPanel = withStyles({
+  root: {
+    border: '1px solid rgba(0, 0, 0, .125)',
+    boxShadow: 'none',
+    '&:not(:last-child)': {
+      borderBottom: 0,
+    },
+    '&:before': {
+      display: 'none',
+    },
+    '&$expanded': {
+      margin: 'auto',
+    },
+  },
+  expanded: {},
+})(MuiExpansionPanel);
 
 export default function () {
   const classes = useStyles();
 
   const navigate = useNavigate();
 
+  const [quizzes, setQuizzes] = useState()
+
+  useEffect(() => {
+    HttpClient.get('/categories/with_quizzes')
+      .then(response => {
+        setQuizzes(response.data);
+      })
+      .catch(error => {
+        console.error("Fetch Quizzes: ", error);
+      })
+  }, [])
+
   return (
-      <Card className={classes.root}>
-        <CardContent>
-          <Typography className={classes.title} color="textSecondary" gutterBottom>
-            Quiz List
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button variant="outlined" size="small" onClick={()=>{
-            navigate('new');
-          }}>Create New Quiz</Button>
-        </CardActions>
-      </Card>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h4" color="textPrimary" gutterBottom>
+          Quiz List
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="outlined" size="small" onClick={() => {
+          navigate('new');
+        }}>Create New Quiz</Button>
+      </CardActions>
+      <LinearLoader loading={!quizzes} />
+      {quizzes &&
+        <>
+          {(quizzes as Array<any>).map((value) => {
+            return (
+              <ExpansionPanel key={value._id}>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMore />}
+                >
+                  <Typography variant="h6">{value.title}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                  <List className={classes.list} key={value._id}>
+                    {(value.quizzes as Array<any>).map((quiz) => {
+                      return (
+                        <ListItem key={quiz._id} button disableRipple>
+                          <ListItemText><Typography variant="body1">{quiz.title}</Typography></ListItemText>
+                          <ListItemSecondaryAction>
+                            <ButtonGroup>
+                              <Button variant="outlined" startIcon={<Edit />} color="primary" onClick={()=>navigate(quiz._id)}>Edit</Button>
+                              <Button variant="outlined" startIcon={<Delete />} color="secondary">Delete</Button>
+                            </ButtonGroup>
+                          </ListItemSecondaryAction>
+                        </ListItem>
+                      );
+                    })}
+                  </List>
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            )
+          })}
+        </>
+      }
+    </Card>
   )
 }
